58b0d8836540dd20fb18529a9fde75f4eeb0414c
aserfend <liuchangm7j@vip.qq.com>
2018-06-17 10:03:48 +0800

    新增日期选择

---
+++ 我在干什么/DateSelector.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace Inst
+{
+	partial class DateSelector
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.SuspendLayout();
+			// 
+			// DateSelector
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.White;
+			this.ClientSize = new System.Drawing.Size(413, 250);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "DateSelector";
+			this.Text = "DateSelector";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+	}
+}
---
+++ 我在干什么/DateSelector.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Inst
+{
+	public partial class DateSelector : Form
+	{
+		public DateSelector()
+		{
+			InitializeComponent();
+		}
+	}
+}
